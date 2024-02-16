1f7275ce0b492b5a4f24af4e267812660ed68cc4
Tedi <Tedi@DESKTOP-ULEHU35>
2024-02-16 19:11:13 +0200

    Add Login, logout actions

--- Contacts/Controllers/UserController.cs
+++ Contacts/Controllers/UserController.cs
@@ -56,7 +56,48 @@ namespace Contacts.Controllers
 				return RedirectToAction("Index", "Home");
 			}
 
+			foreach (var err in result.Errors)
+			{
+				ModelState.AddModelError("", err.Description);
+			}
+
+			return View(model);
+		}
+
+		public IActionResult Login()
+		{
 			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			var result = await _signInManager.PasswordSignInAsync(
+					model.UserName,
+					model.Password,
+					true,
+					false);
+
+			if (!result.Succeeded)
+			{
+				ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+				return View(model);
+			}
+
+			return RedirectToAction("Index", "Home");
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Logout()
+		{
+			await _signInManager.SignOutAsync();
+
+			return RedirectToAction("Index", "Home");
 		}
 	}
 }
